653723a7be35c500d21205201464d013932bdbfc
Tim Cooijmans <t.cooijmans@student.maastrichtuniversity.nl>
2014-03-15 19:16:45 +0100

    generate initial configuration

--- state.cpp
+++ state.cpp
@@ -1,16 +1,35 @@
 #include "state.hpp"
+#include "squares.hpp"
 
-State::State() :
-  color_to_move(Color::White)
+State::State()
 {
-  can_castle_kingside.fill(true);
-  can_castle_queenside.fill(true);
-  
   for (size_t i = 0; i < colors::cardinality; i++) {
     for (size_t j = 0; j < pieces::cardinality; j++) {
       board[i][j] = 0;
     }
   }
+
+  using namespace colors;
+  using namespace pieces;
+  using namespace squares;
+
+  board[white][pawn] = a2 | b2 | c2 | d2 | e2 | f2 | g2 | h2;
+  board[black][pawn] = a7 | b7 | c7 | d7 | e7 | f7 | g7 | h7;
+  board[white][knight] = b1 | g1;
+  board[black][knight] = b8 | g8;
+  board[white][bishop] = c1 | f1;
+  board[black][bishop] = c8 | f8;
+  board[white][rook] = a1 | h1;
+  board[black][rook] = a8 | h8;
+  board[white][queen] = d1;
+  board[black][queen] = d8;
+  board[white][king] = e1;
+  board[black][king] = e8;
+
+  can_castle_kingside.fill(true);
+  can_castle_queenside.fill(true);
+
+  color_to_move = colors::white;
 }
 
 State::State(State &that) :
