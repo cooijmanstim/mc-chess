9e315e057263f802295196cb3124bfedd7344579
Tim Cooijmans <t.cooijmans@student.maastrichtuniversity.nl>
2014-03-27 22:10:36 +0100

    fix tests

--- tests/test.cpp
+++ tests/test.cpp
@@ -18,7 +18,7 @@ BOOST_AUTO_TEST_CASE(initial_moves) {
       expected_moves.emplace(from, from +   north, Move::Type::normal);
       expected_moves.emplace(from, from + 2*north, Move::Type::double_push);
     });
-  bitboard::for_each_member(squares::b1 || squares::g1, [&expected_moves](squares::Index from) {
+  bitboard::for_each_member(squares::b1 | squares::g1, [&expected_moves](squares::Index from) {
       expected_moves.emplace(from, from + 2*north + west, Move::Type::normal);
       expected_moves.emplace(from, from + 2*north + east, Move::Type::normal);
     });
@@ -28,11 +28,13 @@ BOOST_AUTO_TEST_CASE(initial_moves) {
 
   std::set<Move> falsenegatives, falsepositives;
   std::set_difference(expected_moves.begin(), expected_moves.end(),
-                      actual_moves.begin(), actual_moves.begin(),
+                      actual_moves.begin(), actual_moves.end(),
                       std::inserter(falsenegatives, falsenegatives.begin()));
   std::set_difference(actual_moves.begin(), actual_moves.end(),
                       expected_moves.begin(), expected_moves.end(),
                       std::inserter(falsepositives, falsepositives.begin()));
-  BOOST_CHECK_MESSAGE(falsenegatives.empty(), "illegal moves generated: " << falsenegatives);
-  BOOST_CHECK_MESSAGE(falsepositives.empty(), "legal moves not generated: " << falsepositives);
+  std::cout << "expected_moves: " << expected_moves << std::endl;
+  std::cout << "actual_moves: " << actual_moves << std::endl;
+  BOOST_CHECK_MESSAGE(falsenegatives.empty(), "legal moves not generated: " << falsenegatives);
+  BOOST_CHECK_MESSAGE(falsepositives.empty(), "illegal moves generated: " << falsepositives);
 }
