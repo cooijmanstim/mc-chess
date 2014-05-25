300a1b4612b9223383874865efefbb2d2339975f
Tim Cooijmans <t.cooijmans@student.maastrichtuniversity.nl>
2014-05-25 09:58:41 +0200

    use doubles for playout result so we can have draws be 1/2 without complications like result_factor; also backpropagating 1*result_factor - result inexplicably resulted in computation getting slower with each additional sample, whereas backpropagating 1 - result (which was a bug) resulted in constant time per sample. crazy\!

--- mcts.cpp
+++ mcts.cpp
@@ -2,12 +2,16 @@
 
 using namespace mcts;
 
-const int loss_value = 0,
-          draw_value = 1,
-           win_value = 2;
-const int result_factor = 2;
-
 Node::Node() {
+}
+
+const double
+  loss_value = 0,
+  draw_value = 0.5,
+  win_value = 1;
+
+Result Node::invert_result(Result result) {
+  return 1 - result;
 }
 
 // use this to instantiate.  it ensures that there is always a shared_ptr
@@ -39,7 +43,7 @@ void Node::sample(State state, boost::mt19937& generator) {
   FarNode node = this->shared_from_this();
   node = node->select(state);
   node = node->expand(state, generator);
-  float result = node->rollout(state, generator);
+  double result = node->rollout(state, generator);
   node->backprop(result);
 }
 
@@ -64,7 +68,7 @@ FarNode Node::expand(State& state, boost::mt19937& generator) {
   }
 }
 
-int Node::rollout(State& state, boost::mt19937& generator) {
+Result Node::rollout(State& state, boost::mt19937& generator) {
 #ifdef MC_EXPENSIVE_RUNTIME_TESTS
   State initial_state(state);
   std::vector<Move> move_history; // for debugging
@@ -86,35 +90,40 @@ int Node::rollout(State& state, boost::mt19937& generator) {
   return *winner == last_player ? win_value : loss_value;
 }
 
-void Node::backprop(int result) {
-  update(result);
-  if (parent)
-    parent->update(invert_result(result));
+void Node::backprop(Result result) {
+  FarNode node = shared_from_this();
+  do {
+    node->update(result);
+    result = invert_result(result);
+    node = node->parent;
+  } while (node);
 }
 
-void Node::update(int result) {
+void Node::update(Result result) {
   total_result += result;
   nvisits++;
 }
 
-float Node::winrate(FarNode child) {
-  return float(child->total_result) / result_factor / child->nvisits;
+double Node::winrate(FarNode child) {
+  assert(child->nvisits > 0);
+  return child->total_result / child->nvisits;
 }
 
-float Node::uct_score(FarNode child) {
+double Node::uct_score(FarNode child) {
   assert(child->parent);
+  assert(child->nvisits > 0);
   return winrate(child) + sqrt(2 * log(child->parent->nvisits) / child->nvisits);
 }
 
-float Node::most_visited(FarNode child) {
+double Node::most_visited(FarNode child) {
   return child->nvisits;
 }
 
-FarNode Node::select_by(std::function<float(FarNode)> key) {
-  float best_value;
+FarNode Node::select_by(std::function<double(FarNode)> key) {
+  double best_value;
   FarNode best_child;
   for (FarNode curr_child: children) {
-    float curr_value = key(curr_child);
+    double curr_value = key(curr_child);
     if (!best_child || best_value < curr_value) {
       best_value = curr_value;
       best_child = curr_child;
@@ -129,12 +138,8 @@ Move Node::best_move() {
   return *best_child->last_move;
 }
 
-int Node::invert_result(int result) {
-  return 1*result_factor - result;
-}
-
 void Node::print_evaluations() {
   for (FarNode child: children) {
-    std::cout << child->nvisits << " " << winrate(child) << " " << uct_score(child);
+    std::cout << *child->last_move << " " << child->nvisits << " " << winrate(child) << " " << uct_score(child) << std::endl;
   }
 }
--- mcts.hpp
+++ mcts.hpp
@@ -8,6 +8,7 @@ namespace mcts {
   class Node;
 
   typedef boost::shared_ptr<Node> FarNode;
+  typedef double Result;
 
   class Node : public boost::enable_shared_from_this<Node> {
     FarNode parent;
@@ -16,7 +17,7 @@ namespace mcts {
     Color last_player;
     std::vector<Move> unexplored_moves;
     std::vector<FarNode> children;
-    size_t total_result;
+    Result total_result;
     size_t nvisits;
 
     Node();
@@ -27,14 +28,14 @@ namespace mcts {
     void sample(State state, boost::mt19937& generator);
     FarNode select(State& state);
     FarNode expand(State& state, boost::mt19937& generator);
-    int rollout(State& state, boost::mt19937& generator);
-    void backprop(int result);
-    void update(int result);
-    static int invert_result(int result);
-    static float uct_score(FarNode child);
-    static float winrate(FarNode child);
-    static float most_visited(FarNode child);
-    FarNode select_by(std::function<float(FarNode)>);
+    Result rollout(State& state, boost::mt19937& generator);
+    void backprop(Result result);
+    void update(Result result);
+    static Result invert_result(Result result);
+    static double uct_score(FarNode child);
+    static double winrate(FarNode child);
+    static double most_visited(FarNode child);
+    FarNode select_by(std::function<double(FarNode)>);
     Move best_move();
     void print_evaluations();
   };
