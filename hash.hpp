3cf0dbc2884de199b9285787b9cc9f93841f026b
Tim Cooijmans <t.cooijmans@student.maastrichtuniversity.nl>
2014-07-15 21:50:54 +0200

    put hashes inside a data structure to ease serialization

--- hash.hpp
+++ hash.hpp
@@ -1,3 +1,5 @@
+#pragma once
+
 #include <cstdint>
 
 #include <boost/random.hpp>
@@ -12,12 +14,66 @@
 typedef uint64_t Hash;
 
 namespace hashes {
-  Hash generate_random_feature();
+  struct Hashes {
+    Hash black_to_move;
+    array3d<Hash, squares::cardinality, colors::cardinality, pieces::cardinality> colored_piece_at_square;
+    array2d<Hash, colors::cardinality, castles::cardinality> can_castle;
+    std::array<Hash, files::cardinality> en_passant;
+  };
 
-  Hash black_to_move();
-  Hash colored_piece_at_square(const Color color, const Piece piece, const squares::Index square);
-  Hash can_castle(const Color color, const Castle castle);
-  Hash en_passant(const Bitboard en_passant_square);
+  inline Hash generate_random_feature() {
+    static boost::random::mt19937 base_generator;
+    static boost::random::uniform_int_distribution<Hash> distribution;
+    static boost::variate_generator<boost::mt19937&, boost::random::uniform_int_distribution<Hash>> generator(base_generator, distribution);
+    return generator();
+  }
 
-  void toggle(Hash& hash, const Color color, const Piece piece, const squares::Index square);
+  inline const Hashes& get_hashes() {
+    const static Hashes hashes = {
+      generate_random_feature(),
+      []() {
+        array3d<Hash, squares::cardinality, colors::cardinality, pieces::cardinality> x;
+        for (squares::Index s: squares::indices)
+          for (Color c: colors::values)
+            for (Piece p: pieces::values)
+              x[s][c][p] = generate_random_feature();
+        return x;
+      }(),
+      []() {
+        array2d<Hash, colors::cardinality, castles::cardinality> x;
+        for (Color color: colors::values)
+          for (Castle castle: castles::values)
+            x[color][castle] = generate_random_feature();
+        return x;
+      }(),
+      []() {
+        std::array<Hash, files::cardinality> x;
+        for (files::Index file: files::indices)
+          x[file] = generate_random_feature();
+        return x;
+      }(),
+    };
+    return hashes;
+  }
+
+  inline Hash black_to_move() {
+    return get_hashes().black_to_move;
+  }
+  
+  inline Hash colored_piece_at_square(const Color color, Piece piece, squares::Index square) {
+    return get_hashes().colored_piece_at_square[square][color][piece];
+  }
+  
+  inline Hash can_castle(Color color, Castle castle) {
+    return get_hashes().can_castle[color][castle];
+  }
+  
+  inline Hash en_passant(Bitboard en_passant_square) {
+    files::Index file = files::by_square(squares::index(en_passant_square));
+    return get_hashes().en_passant[file];
+  }
+  
+  inline void toggle(Hash& hash, Color color, Piece piece, squares::Index square) {
+    hash ^= colored_piece_at_square(color, piece, square);
+  }
 }
